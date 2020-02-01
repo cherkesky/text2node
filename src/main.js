3284d12b7497d1e48b8ce32714285100bc5a363f
Cherkesky <guy@cherkesky.com>
2020-02-01 15:00:54 -0600

    gitignore and apikeys.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@
 
 var express = require('express');
 var app = express();
+const accountSid = 'ACec7734a550fbbe92ed1c44e99f23a57f';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
 
 app.get('/', function (req, res) {
   res.send('Text2Node');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Listening on port 3000');
 });
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
